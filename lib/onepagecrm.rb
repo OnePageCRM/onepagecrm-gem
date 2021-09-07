339f519405d1e52061eecfb99f5daa831dae1bb4
kevGalway <kev9229@gmail.com>
2021-09-07 10:44:11 +0100

    update to new auth method

--- lib/onepagecrm.rb
+++ lib/onepagecrm.rb
@@ -5,24 +5,22 @@ require 'faraday'
 
 # OnePageCRM API Client Object
 class OnePageCRM
-  def initialize(login = nil, password = nil)
-    @url = 'https://app.onepagecrm.com/api/v3/'
-    @conn = Faraday.new(url: @url)
-    log_in(login, password)
-  end
-
-  # Log user into API
-  def log_in(login, password)
-    params = { login: login, password: password }
-    auth_data = post('login.json', params)
-    @uid = auth_data['data']['user_id']
-    @auth_key = Base64.decode64(auth_data['data']['auth_key'])
+  def initialize(user_id, api_key)
+    url = 'https://app.onepagecrm.com/api/v3/'
+    @conn = Faraday.new(url: url) do |conn|
+      conn.adapter Faraday.default_adapter
+      conn.basic_auth(user_id, api_key)
+      conn.headers.merge!(
+        'Content-Type' => 'application/json',
+        'X-OnePageCRM-UID' => user_id,
+        'X-OnePageCRM-Source' => 'ruby_gem'
+      )
+    end
   end
 
   # defines get and delete methods
   %w(get delete).each do |http_method|
     define_method http_method do |endpoint, params = {}|
-      @conn.headers = auth_header(http_method, @url + endpoint, params)
       response = @conn.send(http_method, endpoint, params)
       JSON.parse response.body
     end
@@ -31,36 +29,8 @@ class OnePageCRM
   # Defines put and post methods
   %w(post put).each do |http_method|
     define_method http_method do |endpoint, params = {}|
-      @conn.headers = auth_header(http_method, @url + endpoint, params)
       response = @conn.send(http_method, endpoint, params.to_json)
       JSON.parse response.body
     end
-  end
-
-  # Returns headers required for API authentication
-  def auth_header(http_method, url, parameters)
-    http_method = http_method.upcase
-    op_headers = { 'Content-Type' => 'application/json' }
-    return op_headers if @uid.nil? || @auth_key.nil?
-    if parameters.any? && %w(GET DELETE).include?(http_method)
-      url += '?' + URI.encode_www_form(parameters)
-    end
-
-    timestamp = Time.now.to_i.to_s
-    token = create_auth_token(http_method, url, parameters, timestamp)
-    { 'X-OnePageCRM-UID' => @uid,
-      'X-OnePageCRM-TS' => timestamp,
-      'X-OnePageCRM-Auth' => token,
-      'X-OnePageCRM-Source' => 'ruby_gem'
-     }.merge op_headers
-  end
-
-  # Creates the auth token for the X-OnePageCRM-Auth header
-  def create_auth_token(http_method, request_url, request_body, timestamp)
-    request_url_hash = Digest::SHA1.hexdigest request_url
-    request_body_hash = Digest::SHA1.hexdigest request_body.to_json
-    signature = [@uid, timestamp, http_method.upcase, request_url_hash].join '.'
-    signature += ('.' + request_body_hash) if %w(POST PUT).include?(http_method)
-    OpenSSL::HMAC.hexdigest('sha256', @auth_key, signature).to_s
   end
 end
